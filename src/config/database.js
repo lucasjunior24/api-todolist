8ea50a7dae716b0e59d4bb693a28ba9d0903e092
lucasjunior24 <lukateku@gmail.com>
2021-01-26 21:40:17 -0300

    deploy

--- src/config/database.js
+++ src/config/database.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect('mongodb+srv://deploy:<password>@cluster0.jy1ah.mongodb.net/<dbname>?retryWrites=true&w=majority')
