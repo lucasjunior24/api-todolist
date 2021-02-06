0d45be2fd0e2ffaa2694f9280c814a4e78c19887
lucasjunior24 <lukateku@gmail.com>
2021-02-06 16:44:07 -0300

    habilitando cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = require('./Utils/router')
 const mongoose = require('mongoose')
 require('dotenv').config()
+const cors = require('cors')
 
 const mongoConnection = process.env.MONGO_URL
 const PORT = process.env.PORT || 3003
@@ -13,7 +14,9 @@ mongoose.connect(mongoConnection, {
     useNewUrlParser: true,
 }, () => console.log('connectado ao banco'))
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
+
 
 app.listen(PORT, () => console.log('Servido rodando na porta 3003'))
