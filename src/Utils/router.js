bb63e4159a94699ccdee32213ab064c6eaa4c5d5
lucasjunior24 <lukateku@gmail.com>
2021-02-06 16:24:41 -0300

    Add controller TODO

--- src/Utils/router.js
+++ src/Utils/router.js
@@ -1,11 +1,11 @@
 const { Router } = require('express')
-const UserController = require('../Controllers/TodoController') 
+const TodoController = require('../Controllers/TodoController') 
 const router = Router()
 
 // Criar todo
-router.post('/todos', UserController.createTodo)
+router.post('/todos', TodoController.createTodo)
 // Listar usuarios
-router.get('/todos', UserController.listTodo)
+router.get('/todos', TodoController.listTodo)
 
 router.get('/', (req, res) => {
     return res.send('Hellor word')
